af0b5b034ba98516ebf37c401f172bc2290b7954
Utilka <46822103+Utilka@users.noreply.github.com>
2023-06-08 14:44:36 +0300

    add rare resource rolltables

--- SystemClassRollTables.py
+++ SystemClassRollTables.py
@@ -38,6 +38,77 @@ class SystemModifier:
 
 
 def create_rr_rolltables():
+    universal_rr = [
+        SystemModifier("Ionic Crystal", "", ""),
+        SystemModifier("Giga Lattice", "", ""),
+        SystemModifier("Amianthoid", "", ""),
+        SystemModifier("Mercurite", "", ""),
+        SystemModifier("Beryllium", "", ""),
+    ]
+    unique_bio_rr = [
+        SystemModifier("RedSang", "", ""),
+        SystemModifier("Bluecap Mold", "", ""),
+        SystemModifier("Eden Incense", "", ""),
+        SystemModifier("Transvine", "", ""),
+        SystemModifier("Superspuds", "", ""),
+        SystemModifier("Proto-Orchid", "", ""),
+        SystemModifier("Proto-Spores", "", ""),
+        SystemModifier("placeholder 01", "", ""),
+        SystemModifier("placeholder 02", "", ""),
+    ]
+    red_rr = [
+        SystemModifier("placeholder 11", "", ""),
+        SystemModifier("placeholder 12", "", ""),
+    ]
+    grey_rr = [
+        SystemModifier("placeholder 21", "", ""),
+        SystemModifier("placeholder 22", "", ""),
+    ]
+
+    universal_rr_rolltable = RollTable.RollTable(options=[RollTable.Option(i, 1) for i in universal_rr])
+    unique_bio_rr_rolltable = RollTable.RollTable(options=[RollTable.Option(i, 1) for i in unique_bio_rr])
+    red_rr_rolltable = RollTable.RollTable(options=[RollTable.Option(i, 1) for i in red_rr])
+    grey_rr_rolltable = RollTable.RollTable(options=[RollTable.Option(i, 1) for i in grey_rr])
+
+    fertile_planet_rolltable = RollTable.RollTable(options=[
+        RollTable.Option(None, 40)],
+        rolltables=[
+            RollTable.Option(unique_bio_rr_rolltable, 39),
+            RollTable.Option(universal_rr_rolltable, 1),
+        ])
+
+    green_class_rolltable = RollTable.RollTable(options=[
+        RollTable.Option(None, 40)],
+        rolltables=[
+            RollTable.Option(universal_rr_rolltable, 1),
+        ])
+
+    orange_class_rolltable = RollTable.RollTable(options=[
+        RollTable.Option(None, 20)],
+        rolltables=[
+            RollTable.Option(universal_rr_rolltable, 1),
+        ])
+
+    red_class_rolltable = RollTable.RollTable(options=[
+        RollTable.Option(None, 10)],
+        rolltables=[
+            RollTable.Option(red_rr_rolltable, 1),
+        ])
+
+    grey_class_rolltable = RollTable.RollTable(options=[
+        RollTable.Option(None, 5)],
+        rolltables=[
+            RollTable.Option(grey_rr_rolltable, 2),
+        ])
+
+    rr_rolltables = {
+        "fertile": fertile_planet_rolltable,
+        "green": green_class_rolltable,
+        "orange": orange_class_rolltable,
+        "red": red_class_rolltable,
+        "grey": grey_class_rolltable,
+    }
+    return rr_rolltables
     pass
 
 
@@ -154,6 +225,13 @@ def create_mod_rolltables():
 
 
 def main():
+    rr_rolltables = create_rr_rolltables()
+    print([str(rr_rolltables["fertile"]()) for i in range(10)])
+    print([str(rr_rolltables["green"]()) for i in range(10)])
+    print([str(rr_rolltables["orange"]()) for i in range(10)])
+    print([str(rr_rolltables["red"]()) for i in range(10)])
+    print([str(rr_rolltables["grey"]()) for i in range(10)])
+
     mod_rolltables = create_mod_rolltables()
     print([str(mod_rolltables["fertile"]()) for i in range(10)])
     print([str(mod_rolltables["green"]()) for i in range(10)])
